1151a0f45aaf3b2d563318af9dd010a10bd31ecd
THTwalker <sshiven19@gmail.com>
2025-03-13 16:25:51 +0530

    Resolved merge conflicts

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,10 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-<<<<<<< HEAD
-  cout << "I have successfully built and run this and this is very hardcd \n";
-=======
-  cout << "I have successfully built and run this  \n";
->>>>>>> c1baf2913081e6d1f1c5df3113990f4ae6edfc44
+  cout << "I have successfully built and run this and this is very hard the code is not working\n";
   return 0;
 }
